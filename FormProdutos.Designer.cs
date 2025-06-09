10db7ceb935e3ce383e163b34521b16a8f0c2d16
Ronaldo Silva Nogueira Batista <ronaldosnbatista@gmail.com>
2025-06-08 23:19:40 -0300

    criação design de cadastro de produtos

---
+++ FormProdutos.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace P2
+{
+    partial class FormProdutos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btncadastrar = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button2 = new Button();
+            btnAtualizar = new Button();
+            btnBuscar = new Button();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(326, 56);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(462, 340);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F);
+            label1.Location = new Point(12, 125);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Descrição";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F);
+            label2.Location = new Point(12, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Preço";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F);
+            label3.Location = new Point(12, 59);
+            label3.Name = "label3";
+            label3.Size = new Size(134, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Nome do produto ";
+            // 
+            // btncadastrar
+            // 
+            btncadastrar.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btncadastrar.Location = new Point(12, 164);
+            btncadastrar.Name = "btncadastrar";
+            btncadastrar.Size = new Size(91, 35);
+            btncadastrar.TabIndex = 4;
+            btncadastrar.Text = "Cadastrar";
+            btncadastrar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 11.25F);
+            textBox1.Location = new Point(84, 122);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(236, 27);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 11.25F);
+            textBox2.Location = new Point(55, 89);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(265, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 11.25F);
+            textBox3.Location = new Point(140, 56);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(180, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(326, 402);
+            button2.Name = "button2";
+            button2.Size = new Size(110, 36);
+            button2.TabIndex = 8;
+            button2.Text = "Deletar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // btnAtualizar
+            // 
+            btnAtualizar.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAtualizar.Location = new Point(206, 164);
+            btnAtualizar.Name = "btnAtualizar";
+            btnAtualizar.Size = new Size(91, 35);
+            btnAtualizar.TabIndex = 9;
+            btnAtualizar.Text = "Atualizar";
+            btnAtualizar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnBuscar.Location = new Point(109, 164);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(91, 35);
+            btnBuscar.TabIndex = 10;
+            btnBuscar.Text = "Buscar";
+            btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(275, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(161, 25);
+            label4.TabIndex = 11;
+            label4.Text = "CRUD PRODUTOS";
+            // 
+            // FormProdutos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(btnBuscar);
+            Controls.Add(btnAtualizar);
+            Controls.Add(button2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(btncadastrar);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "FormProdutos";
+            Text = "FormProdutos";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btncadastrar;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button2;
+        private Button btnAtualizar;
+        private Button btnBuscar;
+        private Label label4;
+    }
+}
