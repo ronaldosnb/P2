0e8c5f5e2931d22cf9770557e5d39e96ae6f7611
Ronaldo Silva Nogueira Batista <ronaldosnbatista@gmail.com>
2025-06-11 23:31:28 -0300

    alterações no cadastro de produtos

--- Forms/FormPedidos.Designer.cs
+++ Forms/FormPedidos.Designer.cs
@@ -60,79 +60,79 @@
             // 
             // txtQuantidade
             // 
-            txtQuantidade.Location = new Point(129, 382);
+            txtQuantidade.Location = new Point(175, 323);
             txtQuantidade.Margin = new Padding(3, 4, 3, 4);
             txtQuantidade.Name = "txtQuantidade";
-            txtQuantidade.Size = new Size(142, 32);
+            txtQuantidade.Size = new Size(146, 27);
             txtQuantidade.TabIndex = 0;
             // 
             // mskCpf
             // 
-            mskCpf.Location = new Point(105, 150);
+            mskCpf.Location = new Point(175, 121);
             mskCpf.Margin = new Padding(3, 4, 3, 4);
-            mskCpf.Mask = "000.000.000-00";
+            mskCpf.Mask = "000,000,000-00";
             mskCpf.Name = "mskCpf";
-            mskCpf.Size = new Size(141, 32);
+            mskCpf.Size = new Size(146, 27);
             mskCpf.TabIndex = 1;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(84, 9);
+            label1.Location = new Point(126, 9);
             label1.Name = "label1";
-            label1.Size = new Size(186, 32);
+            label1.Size = new Size(147, 25);
             label1.TabIndex = 2;
             label1.Text = "Montar pedido";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(17, 385);
+            label2.Location = new Point(63, 326);
             label2.Name = "label2";
-            label2.Size = new Size(115, 25);
+            label2.Size = new Size(90, 20);
             label2.TabIndex = 3;
             label2.Text = "Quantidade:";
             // 
             // lblNomeCliente
             // 
             lblNomeCliente.AutoSize = true;
-            lblNomeCliente.Location = new Point(105, 186);
+            lblNomeCliente.Location = new Point(179, 157);
             lblNomeCliente.Name = "lblNomeCliente";
-            lblNomeCliente.Size = new Size(188, 25);
+            lblNomeCliente.Size = new Size(141, 20);
             lblNomeCliente.TabIndex = 4;
             lblNomeCliente.Text = "----------------------";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(25, 182);
+            label4.Location = new Point(99, 153);
             label4.Name = "label4";
-            label4.Size = new Size(75, 25);
+            label4.Size = new Size(58, 20);
             label4.TabIndex = 5;
             label4.Text = "Cliente:";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(7, 349);
+            label5.Location = new Point(53, 290);
             label5.Name = "label5";
-            label5.Size = new Size(127, 25);
+            label5.Size = new Size(100, 20);
             label5.TabIndex = 6;
             label5.Text = "Cod. produto:";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(2, 149);
+            label6.Location = new Point(76, 120);
             label6.Name = "label6";
-            label6.Size = new Size(109, 25);
+            label6.Size = new Size(84, 20);
             label6.TabIndex = 7;
             label6.Text = "CPF cliente:";
             // 
             // btnBuscarCliente
             // 
-            btnBuscarCliente.Location = new Point(109, 219);
+            btnBuscarCliente.Location = new Point(183, 190);
             btnBuscarCliente.Name = "btnBuscarCliente";
             btnBuscarCliente.Size = new Size(138, 37);
             btnBuscarCliente.TabIndex = 8;
@@ -143,17 +143,17 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(88, 81);
+            label7.Location = new Point(162, 88);
             label7.Name = "label7";
-            label7.Size = new Size(153, 25);
+            label7.Size = new Size(122, 20);
             label7.TabIndex = 10;
             label7.Text = "Dados do cliente";
             // 
             // btnAdicionarProduto
             // 
-            btnAdicionarProduto.Location = new Point(129, 416);
+            btnAdicionarProduto.Location = new Point(175, 357);
             btnAdicionarProduto.Name = "btnAdicionarProduto";
-            btnAdicionarProduto.Size = new Size(142, 34);
+            btnAdicionarProduto.Size = new Size(146, 34);
             btnAdicionarProduto.TabIndex = 13;
             btnAdicionarProduto.Text = "Adicionar produto";
             btnAdicionarProduto.UseVisualStyleBackColor = true;
@@ -161,7 +161,7 @@
             // 
             // btnSalvarPedido
             // 
-            btnSalvarPedido.Location = new Point(374, 508);
+            btnSalvarPedido.Location = new Point(415, 232);
             btnSalvarPedido.Name = "btnSalvarPedido";
             btnSalvarPedido.Size = new Size(140, 38);
             btnSalvarPedido.TabIndex = 14;
@@ -172,68 +172,68 @@
             // lblTotalPedido
             // 
             lblTotalPedido.AutoSize = true;
-            lblTotalPedido.Location = new Point(571, 511);
+            lblTotalPedido.Location = new Point(612, 235);
             lblTotalPedido.Name = "lblTotalPedido";
-            lblTotalPedido.Size = new Size(148, 25);
+            lblTotalPedido.Size = new Size(111, 20);
             lblTotalPedido.TabIndex = 15;
             lblTotalPedido.Text = "-----------------";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(520, 511);
+            label8.Location = new Point(561, 235);
             label8.Name = "label8";
-            label8.Size = new Size(56, 25);
+            label8.Size = new Size(45, 20);
             label8.TabIndex = 16;
             label8.Text = "Total:";
             // 
             // txtCodigoPedido
             // 
-            txtCodigoPedido.Location = new Point(105, 116);
+            txtCodigoPedido.Location = new Point(175, 47);
             txtCodigoPedido.Name = "txtCodigoPedido";
-            txtCodigoPedido.Size = new Size(142, 32);
+            txtCodigoPedido.Size = new Size(146, 27);
             txtCodigoPedido.TabIndex = 19;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(22, 116);
+            label3.Location = new Point(59, 50);
             label3.Name = "label3";
-            label3.Size = new Size(77, 25);
+            label3.Size = new Size(113, 20);
             label3.TabIndex = 20;
-            label3.Text = "Código:";
+            label3.Text = "Código pedido:";
             // 
             // dataGridItens
             // 
             dataGridItens.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridItens.Location = new Point(374, 323);
+            dataGridItens.Location = new Point(415, 47);
             dataGridItens.Name = "dataGridItens";
             dataGridItens.RowHeadersWidth = 51;
-            dataGridItens.Size = new Size(366, 179);
+            dataGridItens.Size = new Size(512, 179);
             dataGridItens.TabIndex = 21;
             // 
             // txtCodigoProduto
             // 
-            txtCodigoProduto.Location = new Point(132, 350);
+            txtCodigoProduto.Location = new Point(175, 291);
             txtCodigoProduto.Name = "txtCodigoProduto";
-            txtCodigoProduto.Size = new Size(139, 32);
+            txtCodigoProduto.Size = new Size(146, 27);
             txtCodigoProduto.TabIndex = 22;
             // 
             // dataProdutos
             // 
             dataProdutos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataProdutos.Location = new Point(374, 77);
+            dataProdutos.Location = new Point(11, 429);
             dataProdutos.Name = "dataProdutos";
             dataProdutos.RowHeadersWidth = 51;
-            dataProdutos.Size = new Size(366, 196);
+            dataProdutos.Size = new Size(350, 183);
             dataProdutos.TabIndex = 23;
             // 
             // pedido
             // 
             pedido.AutoSize = true;
-            pedido.Location = new Point(472, 295);
+            pedido.Location = new Point(597, 24);
             pedido.Name = "pedido";
-            pedido.Size = new Size(154, 25);
+            pedido.Size = new Size(121, 20);
             pedido.TabIndex = 24;
             pedido.Text = "Pedido na sacola";
             pedido.Click += pedido_Click;
@@ -241,35 +241,35 @@
             // label10
             // 
             label10.AutoSize = true;
-            label10.Location = new Point(508, 49);
+            label10.Location = new Point(154, 406);
             label10.Name = "label10";
-            label10.Size = new Size(88, 25);
+            label10.Size = new Size(68, 20);
             label10.TabIndex = 25;
             label10.Text = "Produtos";
             // 
             // label11
             // 
             label11.AutoSize = true;
-            label11.Location = new Point(88, 313);
+            label11.Location = new Point(162, 253);
             label11.Name = "label11";
-            label11.Size = new Size(174, 25);
+            label11.Size = new Size(137, 20);
             label11.TabIndex = 26;
             label11.Text = "Adicionar produtos";
             // 
             // dataPedidos
             // 
             dataPedidos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataPedidos.Location = new Point(801, 77);
+            dataPedidos.Location = new Point(415, 357);
             dataPedidos.Name = "dataPedidos";
             dataPedidos.RowHeadersWidth = 51;
-            dataPedidos.Size = new Size(417, 425);
+            dataPedidos.Size = new Size(512, 219);
             dataPedidos.TabIndex = 27;
             // 
             // btnExcluir
             // 
-            btnExcluir.Location = new Point(801, 503);
+            btnExcluir.Location = new Point(415, 582);
             btnExcluir.Name = "btnExcluir";
-            btnExcluir.Size = new Size(105, 33);
+            btnExcluir.Size = new Size(123, 30);
             btnExcluir.TabIndex = 28;
             btnExcluir.Text = "Excluir";
             btnExcluir.UseVisualStyleBackColor = true;
@@ -278,17 +278,17 @@
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(946, 49);
+            label9.Location = new Point(597, 334);
             label9.Name = "label9";
-            label9.Size = new Size(160, 25);
+            label9.Size = new Size(126, 20);
             label9.TabIndex = 29;
             label9.Text = "Pedidos gravados";
             // 
             // FormPedidos
             // 
-            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1276, 637);
+            ClientSize = new Size(1100, 637);
             Controls.Add(label9);
             Controls.Add(btnExcluir);
             Controls.Add(dataPedidos);
